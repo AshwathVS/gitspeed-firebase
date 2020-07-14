750ba6afe119f4ca44bdee21e696095b7db7e5db
Ashwath V S <ashsat.vijayan122@gmail.com>
2020-07-14 17:46:33 +0100

    changes to implementation and env steup

--- functions/index.js
+++ functions/index.js
@@ -26,10 +26,27 @@ const { user } = require("firebase-functions/lib/providers/auth");
 const githubAccessTokenEndPoint = "https://github.com/login/oauth/access_token";
 const githubUserProfileEndPoint = "https://api.github.com/user";
 
+const getClientId = function() {
+  return apiKeys[apiKeys.env].CLIENT_ID;
+}
+
+const getClientSecret = function() {
+  return apiKeys[apiKeys.env].CLIENT_SECRET;
+}
+
+// server test
 app.get("/hello", (req, res) => {
-  return res.status(200).send("Hello from gitti-space");
+  return res.status(200).send("Hello from gitspeed");
 });
 
+// redirect link
+app.get("/oauth", (req, res) => {
+  return res
+    .status(200)
+    .send("Please close this tab and continue from the extension.");
+});
+
+// fetch user token
 app.post("/fetch-user-token", (req, res) => {
   if (!req.body.code || !req.body.state) {
     return res.status(422).send("Invalid request");
@@ -42,8 +59,8 @@ app.post("/fetch-user-token", (req, res) => {
     axios
       .post(githubAccessTokenEndPoint, null, {
         params: {
-          client_id: apiKeys.CLIENT_ID,
-          client_secret: apiKeys.CLIENT_SECRET,
+          client_id: getClientId(),
+          client_secret: getClientSecret(),
           code: code,
           state: state,
         },
@@ -51,19 +68,21 @@ app.post("/fetch-user-token", (req, res) => {
       .then((resp) => {
         const data = resp.data.split("&");
         var isResponseValid = false;
-        var result = {};
+        var authResult = {};
         data.forEach((element) => {
           const entry = element.split("=");
           if (entry[0] === "access_token") isResponseValid = true;
 
-          result[entry[0]] = entry[1];
+          authResult[entry[0]] = entry[1];
         });
 
-        const access_token = result.access_token;
         // if there is no access token, then there is some error, so return the error data
         if (!isResponseValid) {
-          return res.status(420).send(result);
+          return res.status(420).send(authResult);
         } else {
+          // get access token
+          const access_token = authResult.access_token;
+
           // use the access token to fetch the user details from github to save into firestore
           axios
             .get(githubUserProfileEndPoint, {
@@ -73,33 +92,27 @@ app.post("/fetch-user-token", (req, res) => {
             })
             .then((resp) => {
               const userData = resp.data;
+              const docId = userData.login;
+              const dbUser = {
+                name: userData.name,
+                avatar_url: userData.avatar_url,
+                email: userData.email,
+                location: userData.location,
+                access_token: access_token,
+              };
 
-              // check if user already exists
+              // set will create or update(if document already exists)
               db.collection("users")
-                .where("username", "==", userData.login)
-                .get()
-                .then((snapshot) => {
-                  // update access_token if user already exists
-                  if (!snapshot.empty) {
-                    snapshot.forEach((doc) => {
-                      db.collection("users")
-                        .doc(doc.id)
-                        .update({ access_token: access_token });
-                    });
-                  }
-                  // create new user if not
-                  else {
-                    db.collection("users").add({
-                      username: userData.login,
-                      name: userData.name,
-                      avatar_url: userData.avatar_url,
-                      email: userData.name,
-                      location: userData.location,
-                      access_token: access_token,
-                    });
-                  }
+                .doc(docId)
+                .set(dbUser)
+                .then((resp) => {
+                  functions.logger.info("User add sucessful: " + docId);
+                  dbUser.username = docId;
+                  res.status(200).send({ user: dbUser });
+                })
+                .catch((error) => {
+                  functions.logger.error(error);
                 });
-              return res.status(200).send(result);
             })
             .catch((error) => {
               functions.logger.error(error);
